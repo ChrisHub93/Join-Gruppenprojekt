9b9d370203208bfbc378c11bf7daa02bd616da7b
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-05-21 15:43:29 +0200

    index.js: added log in functions

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+async function logIn() {
+    let data = await loadData("/users");
+    let dataValues = Object.values(data);
+    // console.log("data:", data);
+    console.log("dataValues:", dataValues);
+    search(dataValues)
+    
+}
+
+async function loadData(path = "") {
+  let response = await fetch(BASE_URL + path + ".json");
+
+  let responseToJson = await response.json();
+  return responseToJson;
+}
+
+function search(dataValues) {
+    email = document.getElementById("inputEmail").value;
+    password = document.getElementById("inputPassword").value;
+
+    for (listOfUser of dataValues) {
+        let mails = listOfUser.email;
+        let passwords = listOfUser.password
+
+        if (mails == email && passwords == password) {
+            console.log("stimmt");
+            window.location.href = '../html/summary.html'
+            return
+        } else {
+            console.log("keine Übereinstimmung");
+        }
+        
+      }
+
+}
