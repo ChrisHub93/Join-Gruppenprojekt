99938033599f1c5637a0bc466bad842e42327e79
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-05-21 17:56:49 +0200

    index.js: removed console log´s

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,31 @@
 async function logIn() {
-    let data = await loadData("/users");
-    let dataValues = Object.values(data);
-    // console.log("data:", data);
-    console.log("dataValues:", dataValues);
-    search(dataValues)
-    
+  let data = await loadData("/users");
+  let dataValues = Object.values(data);
+  
+  search(dataValues);
 }
 
 async function loadData(path = "") {
   let response = await fetch(BASE_URL + path + ".json");
-
   let responseToJson = await response.json();
+
   return responseToJson;
 }
 
 function search(dataValues) {
-    email = document.getElementById("inputEmail").value;
-    password = document.getElementById("inputPassword").value;
+  email = document.getElementById("inputEmail").value;
+  password = document.getElementById("inputPassword").value;
 
-    for (listOfUser of dataValues) {
-        let mails = listOfUser.email;
-        let passwords = listOfUser.password
+  for (listOfUser of dataValues) {
+    let mails = listOfUser.email;
+    let passwords = listOfUser.password;
 
-        if (mails == email && passwords == password) {
-            console.log("stimmt");
-            window.location.href = '../html/summary.html'
-            return
-        } else {
-            console.log("keine Übereinstimmung");
-        }
-        
-      }
-
+    if (mails == email && passwords == password) {
+      console.log("stimmt");
+      window.location.href = "../html/summary.html";
+      return;
+    } else {
+      console.log("keine Übereinstimmung");
+    }
+  }
 }
