4df5d6a42dbc0a7651e4c90346989901a4bb9981
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-05-21 14:16:07 +0200

    sign_up: added test load to server

--- scripts/sign_up.js
+++ scripts/sign_up.js
@@ -1,3 +1,27 @@
-function initSignUp() {
-    
+let name = "";
+let email = "";
+let password = "";
+let passwordConfirm = "";
+
+function initSignUp() {}
+
+function submit() {
+  name = document.getElementById("inputName").value;
+  email = document.getElementById("inputEmail").value;
+  password = document.getElementById("inputPassword").value;
+  passwordConfirm = document.getElementById("inputePasswordConfirm").value;
+  postData("/users/", {email: email, password : password});
+  window.location.href = '../index.html'
 }
+
+async function postData(path, data={}) {
+    let response = await fetch(BASE_URL + path + ".json",{
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(data)
+    });
+    return responseToJson = await response.json();
+}
+
