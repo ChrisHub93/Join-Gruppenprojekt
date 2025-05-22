9131b210a81597a0a416060b1715dc0298d7c737
xCnKenan <cn.kenan@icloud.com>
2025-05-22 21:35:34 +0200

    add_task.html: Content failure when integrate basic framework

--- scripts/templates.js
+++ scripts/templates.js
@@ -25,32 +25,19 @@ function getSidebar() {
 }
 
 function getSubTasksTemplate(inputRef) {
-  return `
-            
-
-            <div id="${inputRef.value}" class="relative">
+  return `<div id="${inputRef.value}" class="relative">
             <div id="bullet${inputRef.value}" class="bullet"></div>
+                <input onclick="editTask('${inputRef.value}')" type ="text" value="${inputRef.value}"/>
 
-  <input onclick="editTask('${inputRef.value}')" type ="text" value="${inputRef.value}"/>
-
-    <div id="editOrTrash" class="editOrTrash">
-
-      <img onclick="editTask('${inputRef.value}')" src="/assets/icons/Property 1=edit.png" alt="">
-
-      <div class="subTasksSeperatorSecond"></div>
-        <img onclick="completeDeleteTask('${inputRef.value}')" src="/assets/icons/Property 1=delete.png" alt="">
-    </div>
-
-    <div id="trashOrCheck" class="editOrTrash d-none">
-
-      <img onclick="completeDeleteTask('${inputRef.value}')" src="/assets/icons/Property 1=delete.png" alt="">
-
-      <div class="subTasksSeperatorSecond"></div>
-
-      <img onclick="acceptTask('${inputRef.value}')" src="/assets/icons/Property 1=check.png" alt="">
-
-    </div>
-              
-                
-  </div>`;
+            <div id="editOrTrash" class="editOrTrash">
+                <img onclick="editTask('${inputRef.value}')" src="/assets/icons/Property 1=edit.png" alt="">
+                    <div class="subTasksSeperatorSecond"></div>
+                <img onclick="completeDeleteTask('${inputRef.value}')" src="/assets/icons/Property 1=delete.png" alt="">
+            </div>
+                <div id="trashOrCheck" class="editOrTrash d-none">
+                    <img onclick="completeDeleteTask('${inputRef.value}')" src="/assets/icons/Property 1=delete.png" alt="">
+                        <div class="subTasksSeperatorSecond"></div>
+                    <img onclick="acceptTask('${inputRef.value}')" src="/assets/icons/Property 1=check.png" alt="">
+                </div>               
+            </div>`;
 }
