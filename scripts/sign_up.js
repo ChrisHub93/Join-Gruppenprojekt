31742c2da608f42ebe9316363ddd176c5b588ae7
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-05-28 14:24:59 +0200

    sign_up: Validate all inputs via JS

--- scripts/sign_up.js
+++ scripts/sign_up.js
@@ -1,12 +1,15 @@
+let nameCheck = false;
+let emailCheck = false;
 let pwCheck = false;
+let pwConfirmCheck = false;
 
 function initSignUp() {
-    pwCheck = false;
+    setChecksToFalse();
 }
 
 async function signUp() {
   validateAllInputs();
-  if (pwCheck) {
+  if (nameCheck && emailCheck && pwCheck && pwConfirmCheck) {
     await getInpuValueAndPost();
     window.location.href = "../index.html";
   } else {
@@ -15,18 +18,17 @@ async function signUp() {
 }
 
 function validateAllInputs() {
+  validateNameInput();
   validateEmailInput();
-  validateInput("inputName", "nameFeedback");
-  validateInput("inputPassword", "passwortFeedback");
-  validateInput("inputePasswordConfirm", "confirmPasswortFeedback")
+  validatePasswordInput();
   validateConfirmPassword();
+  console.log(nameCheck, emailCheck, pwCheck, pwConfirmCheck);
 }
 
 async function getInpuValueAndPost() {
   let name = document.getElementById("inputName").value;
   let email = document.getElementById("inputEmail").value;
   let password = document.getElementById("inputPassword").value;
-  let passwordConfirm = document.getElementById("inputePasswordConfirm").value;
   await postData("/users/", { name: name, email: email, password: password });
 }
 
@@ -41,6 +43,21 @@ async function postData(path, data = {}) {
   return (responseToJson = await response.json());
 }
 
+function validateNameInput() {
+  const nameInputRef = document.getElementById("inputName");
+  const feedbackElementRef = document.getElementById("nameFeedback");
+
+  if (!nameInputRef.value) {
+    nameInputRef.style.border = "1px solid var(--error-color)";
+    feedbackElementRef.textContent = "This Field is required";
+    nameCheck = false;
+  } else {
+    feedbackElementRef.textContent = "";
+    nameInputRef.style.border = "1px solid var(--input-border)";
+    nameCheck = true;
+  }
+}
+
 function validateEmailInput() {
   const emailInputRef = document.getElementById("inputEmail");
   const feedbackElementRef = document.getElementById("emailFeedback");
@@ -49,43 +66,47 @@ function validateEmailInput() {
   if (emailRegex.test(emailInputRef.value)) {
     feedbackElementRef.textContent = "";
     emailInputRef.style.border = "1px solid var(--input-border)";
+    emailCheck = true;
   } else {
     emailInputRef.style.border = "1px solid var(--error-color)";
     feedbackElementRef.textContent = "Please enter a valid email address.";
+    emailCheck = false;
   }
 }
 
-function validateInput(id, textId) {
-  const pwInputRef = document.getElementById(id);
-  const feedbackElementRef = document.getElementById(textId);
+function validatePasswordInput() {
+  const pwInputRef = document.getElementById("inputPassword");
+  const feedbackElementRef = document.getElementById("passwortFeedback");
 
   if (!pwInputRef.value) {
     pwInputRef.style.border = "1px solid var(--error-color)";
     feedbackElementRef.textContent = "This Field is required";
+    pwCheck = false;
   } else {
     feedbackElementRef.textContent = "";
     pwInputRef.style.border = "1px solid var(--input-border)";
+    pwCheck = true;
   }
 }
 
 function validateConfirmPassword() {
   let pwInput = "";
   let confirmPwInput = "";
-  pwCheck = false;
+  pwConfirmCheck = false;
   pwInput = document.getElementById("inputPassword").value;
-  confirmPwInput = document.getElementById("inputePasswordConfirm").value;
+  confirmPwInput = document.getElementById("inputPasswordConfirm").value;
   if (pwInput === confirmPwInput && confirmPwInput != "") {
     hideUserFeedback();
-    pwCheck = true;
+    pwConfirmCheck = true;
   } else {
-    pwCheck = false;
+    pwConfirmCheck = false;
     clearPasswordInputs();
     showUserFeedback();
   }
 }
 
 function hideUserFeedback() {
-  const confirmPwInputRef = document.getElementById("inputePasswordConfirm");
+  const confirmPwInputRef = document.getElementById("inputPasswordConfirm");
   const feedbackElementRef = document.getElementById("confirmPasswortFeedback");
 
   confirmPwInputRef.style.border = "1px solid var(--input-border)";
@@ -94,13 +115,20 @@ function hideUserFeedback() {
 
 function clearPasswordInputs() {
   document.getElementById("inputPassword").value = "";
-  document.getElementById("inputePasswordConfirm").value = "";
+  document.getElementById("inputPasswordConfirm").value = "";
 }
 
 function showUserFeedback() {
-  const confirmPwInputRef = document.getElementById("inputePasswordConfirm");
+  const confirmPwInputRef = document.getElementById("inputPasswordConfirm");
   const feedbackElementRef = document.getElementById("confirmPasswortFeedback");
   confirmPwInputRef.style.border = "1px solid var(--error-color)";
   feedbackElementRef.textContent =
     "Your passwords don`t match. Please try again";
+}
+
+function setChecksToFalse() {
+  nameCheck = false;
+  emailCheck = false;
+  pwCheck = false;
+  pwConfirmCheck = false;
 }
