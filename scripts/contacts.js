21f6c4ae277293f914b28ea6425d2245dfc41522
xCnKenan <cn.kenan@icloud.com>
2025-05-28 21:35:21 +0200

    contacts.html: loading contacts from databs

---
+++ scripts/contacts.js
@@ -0,0 +1,38 @@
+async function init() {
+  let contacts = await fetchData("/contacts/");
+  let contactsArray = Object.values(contacts);
+  
+  getListOfContacts(contactsArray);
+}
+
+async function fetchData(path) {
+  let response = await fetch(BASE_URL + path + ".json");
+  let responseAsJson = await response.json();
+  return responseAsJson;
+}
+
+function getListOfContacts(contactsArray) {
+  for (user of contactsArray) {
+    let emailOfUser = user.email;
+    let nameOfUser = user.name;
+
+    getTemplate(emailOfUser, nameOfUser);
+  }
+}
+
+function getTemplate(emailOfUser, nameOfUser) {
+  let alphabeticalOrderRef = document.getElementById("alphabeticalOrderA");
+
+  alphabeticalOrderRef.innerHTML += `<span class="order">A</span>
+            <div class="seperator"></div>
+            <div class="contactInfo">
+              <div class="circleFirstLetters">
+                <span>A</span>
+                <span>M</span>
+              </div>
+              <div class="maininfoAboutContact">
+                <span class="name">${nameOfUser}</span>
+                <span class="email">${emailOfUser}</span>
+              </div>
+            </div>`;
+}
