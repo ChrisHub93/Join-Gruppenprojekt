57665679e6c5f8db56157376c358cea51d664eae
Dave Hannemann <david.dikschat@web.de>
2025-06-04 16:47:08 +0200

    some template changing + diff login vs logout + first steps Help

--- scripts/script.js
+++ scripts/script.js
@@ -6,33 +6,35 @@ function init() {
     sidebarVisibility();
 }
 
-// function loginAsGuest() {
-//     sessionStorage.setItem('loginStatus', 'guest');
-//     sidebarVisibility();
-// }
+function loginAsGuest() {
+    sessionStorage.setItem('loginStatus', 'guest');
+}
 
 // function loginAsUser() {
 //     sessionStorage.setItem('loginStatus', 'user');
-//     sidebarVisibility();
 // }
 
 function logout() {
     sessionStorage.setItem('loginStatus', 'none');
-    sidebarVisibility();
 }
 
 function sidebarVisibility() {
     let status = sessionStorage.getItem('loginStatus');
     let isLoggedIn = status === 'user' || status === 'guest';
 
-    document.querySelectorAll('.logged-in').forEach(el =>
-        el.style.display = isLoggedIn ? 'flex' : 'none'
-    );
+    document.querySelectorAll('.logged-in').forEach(el => {
+        el.classList.remove('hidden-init');
+        el.style.display = isLoggedIn ? 'flex' : 'none';
+    });
 
-    document.querySelectorAll('.logged-out').forEach(el =>
-        el.style.display = isLoggedIn ? 'none' : 'flex'
-    );
+    document.querySelectorAll('.logged-out').forEach(el => {
+        el.classList.remove('hidden-init');
+        el.style.display = isLoggedIn ? 'none' : 'flex';
+    });
     setSideBarMenu(isLoggedIn);
+    document.querySelectorAll('.menu-box').forEach(el =>
+        el.classList.add('ready')
+    );
 }
 
 function setSideBarMenu(isLoggedIn) {
@@ -73,36 +75,54 @@ function outsideNavbar() {
 
 function activateMenu(clickedElement, menuKey) {
     sessionStorage.setItem('activeMenu', menuKey);
-    if (clickedElement.classList.contains('policy-text')) {
+    if (clickedElement.classList.contains('policy-text') || clickedElement.classList.contains('navbarlink')) {
         sessionStorage.setItem('activePolicy', menuKey);
     } else {
         sessionStorage.removeItem('activePolicy');
     }
     clearMenu();
-        document.querySelectorAll(`[menu-data="${menuKey}"]`).forEach(menuElement => {
-        menuElement.classList.add('sidebar-menu-active');
-                if (menuElement.classList.contains('policy-text')) {
-            menuElement.classList.add('policy-text-active');
-        }
-    });
+    addMenuactive(menuKey);
 }
 
 function activeMenuStorage() {
+    if (checkBlacklist()) return;
     let activeMenu = sessionStorage.getItem('activeMenu') || 'summary';
     let activePolicy = sessionStorage.getItem('activePolicy');
     clearMenu();
         
         document.querySelectorAll(`[menu-data="${activeMenu}"]`).forEach(menuElement => {
         menuElement.classList.add('sidebar-menu-active');
-        if (activePolicy && menuElement.classList.contains('policy-text')) {
+        if (activePolicy && (menuElement.classList.contains('policy-text') || menuElement.classList.contains('navbarlink'))) {
             menuElement.classList.add('policy-text-active');
         }
     });
 }
 
 function clearMenu() {
-    document.querySelectorAll('.sidebar-menu').forEach(menu => {
+    document.querySelectorAll('.sidebar-menu, .navbarlink, .policy-text').forEach(menu => {
         menu.classList.remove('sidebar-menu-active');
         menu.classList.remove('policy-text-active');
     })
+}
+
+function addMenuactive(menuKey) {
+        document.querySelectorAll(`[menu-data="${menuKey}"]`).forEach(menuElement => {
+        menuElement.classList.add('sidebar-menu-active');
+        if (menuElement.classList.contains('policy-text') || menuElement.classList.contains('navbarlink')) {
+            menuElement.classList.add('policy-text-active');
+        }
+    })
+}
+
+function checkBlacklist() {
+    let excludedPages = ['help.html'];
+    let currentPage = window.location.pathname.split('/').pop();
+
+    if (excludedPages.includes(currentPage)) {
+        clearMenu();
+        sessionStorage.removeItem('activeMenu');
+        sessionStorage.removeItem('activePolicy');
+        return true;
+    }
+    return false;
 }
