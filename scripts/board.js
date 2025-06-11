15c0957d572660dd2b3ccca834ade91cee90d7e1
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-06-11 14:50:23 +0200

    board: drag and drop 1.1

--- scripts/board.js
+++ scripts/board.js
@@ -1,114 +1,135 @@
-let todos = [{
-    'id': 0,
-    'title': 'Test new Task',
-    'category': 'User Story',
-    'description' : 'build with start',
-    'assignedTo' : '',
-    'subTasks' : 'Task1',
-    'priority' : 'medium',
-    'date': '11/11/25',
-    'status':'To do'
-}, {
-    'id': 1,
-    'title': 'Kochwelt Page & Recipe Recommander',
-    'category': 'Technical Task',
-    'description' : 'build with start',
-    'assignedTo' : '',
-    'subTasks' : 'Task1',
-    'priority' : 'medium',
-    'date': '11/11/25',
-    'status':'In progress'
-}, {
-    'id': 2,
-    'title': 'Kochwelt Page & Recipe Recommander',
-    'category': 'User Story',
-    'description' : 'build with start',
-    'assignedTo' : '',
-    'subTasks' : 'Task1',
-    'priority' : 'medium',
-    'date': '11/11/25',
-    'status':'Await feedback'
-}, {
-    'id': 3,
-    'title': 'Kochwelt Page & Recipe Recommander',
-    'category': 'User Story',
-    'description' : 'build with start',
-    'assignedTo' : '',
-    'subTasks' : 'Task1',
-    'priority' : 'medium',
-    'date': '11/11/25',
-    'status':'Done'
-},];
+let todos = [
+  {
+    id: 0,
+    title: "Test new Task",
+    category: "User Story",
+    description: "build with start",
+    assignedTo: "",
+    subTasks: "Task1",
+    priority: "medium",
+    date: "11/11/25",
+    status: "To do",
+  },
+  {
+    id: 1,
+    title: "Kochwelt Page & Recipe Recommander",
+    category: "Technical Task",
+    description: "build with start",
+    assignedTo: "",
+    subTasks: "Task1",
+    priority: "medium",
+    date: "11/11/25",
+    status: "In progress",
+  },
+  {
+    id: 2,
+    title: "Kochwelt Page & Recipe Recommander",
+    category: "User Story",
+    description: "build with start",
+    assignedTo: "",
+    subTasks: "Task1",
+    priority: "medium",
+    date: "11/11/25",
+    status: "Await feedback",
+  },
+  {
+    id: 3,
+    title: "Kochwelt Page & Recipe Recommander",
+    category: "User Story",
+    description: "build with start",
+    assignedTo: "",
+    subTasks: "Task1",
+    priority: "medium",
+    date: "11/11/25",
+    status: "Done",
+  },
+];
 
-function loadTasks(){
-    let toDoContentRef = document.getElementById("toDoContent");
-    let inProgressContentRef = document.getElementById("inProgressContent");
-    let awaitFeedbackContentRef = document.getElementById("awaitFeedbackContent");
-    let doneContentRef = document.getElementById("doneContent");
+function loadTasks() {
+  let toDoContentRef = document.getElementById("toDoContent");
+  let inProgressContentRef = document.getElementById("inProgressContent");
+  let awaitFeedbackContentRef = document.getElementById("awaitFeedbackContent");
+  let doneContentRef = document.getElementById("doneContent");
 
-    let statusToDo = todos.filter(task => task['status'] == 'To do');
-    
-    let statusInProgress = todos.filter(task => task['status'] == 'In progress');
-    let statusAwaitFeedback = todos.filter(task => task['status'] == 'Await feedback');
-    let statusDone = todos.filter(task => task['status'] == 'Done');
+  let statusToDo = todos.filter((task) => task["status"] == "To do");
 
-    toDoContentRef.innerHTML = '';
-    inProgressContentRef.innerHTML = '';
-    awaitFeedbackContentRef.innerHTML = '';
-    doneContentRef.innerHTML = '';
+  let statusInProgress = todos.filter(
+    (task) => task["status"] == "In progress"
+  );
+  let statusAwaitFeedback = todos.filter(
+    (task) => task["status"] == "Await feedback"
+  );
+  let statusDone = todos.filter((task) => task["status"] == "Done");
 
-    for (let index = 0; index < statusToDo.length; index++) {
-        const element = statusToDo[index];
-        toDoContentRef.innerHTML += getTaskTemplate(element);
+  toDoContentRef.innerHTML = "";
+  inProgressContentRef.innerHTML = "";
+  awaitFeedbackContentRef.innerHTML = "";
+  doneContentRef.innerHTML = "";
+
+  for (let index = 0; index < statusToDo.length; index++) {
+    if (statusToDo.length === 0) {
+      toDoContentRef.innerHTML = getEmptyTemplate();
+    } else {
+      const element = statusToDo[index];
+      toDoContentRef.innerHTML += getTaskTemplate(element);
     }
+  }
 
-    for (let index = 0; index < statusInProgress.length; index++) {
-        const element = statusInProgress[index];
-        
-        inProgressContentRef.innerHTML += getTaskTemplate(element);
+  for (let index = 0; index < statusInProgress.length; index++) {
+    if (statusInProgress.length == 0) {
+      inProgressContentRef.innerHTML = getEmptyTemplate();
+    } else {
+      const element = statusInProgress[index];
+      inProgressContentRef.innerHTML += getTaskTemplate(element);
     }
+  }
 
-    for (let index = 0; index < statusAwaitFeedback.length; index++) {
-        const element = statusAwaitFeedback[index];
-
-        awaitFeedbackContentRef.innerHTML += getTaskTemplate(element);
+  for (let index = 0; index < statusAwaitFeedback.length; index++) {
+    if (statusAwaitFeedback.length == 0) {
+      awaitFeedbackContentRef.innerHTML = getEmptyTemplate();
+    } else {
+      const element = statusAwaitFeedback[index];
+      awaitFeedbackContentRef.innerHTML += getTaskTemplate(element);
     }
+  }
 
-    for (let index = 0; index < statusDone.length; index++) {
-        const element = statusDone[index];
-        
-        doneContentRef.innerHTML += getTaskTemplate(element);
+  for (let index = 0; index < statusDone.length; index++) {
+    if (statusDone.length == 0) {
+      doneContentRef.innerHTML = getEmptyTemplate();
+    } else {
+      const element = statusDone[index];
+
+      doneContentRef.innerHTML += getTaskTemplate(element);
     }
-
+  }
 }
 
 let currentDraggedElement;
 
 function startDragging(id) {
-    currentDraggedElement = id;
-    console.log(currentDraggedElement);
-    
+  currentDraggedElement = id;
+  console.log(currentDraggedElement);
 }
 
-function allowDrop(event){
-    event.preventDefault();
+function allowDrop(event) {
+  event.preventDefault();
 }
 
 function moveTo(status) {
-    todos[currentDraggedElement]['status'] = status;
-    loadTasks();
+  todos[currentDraggedElement]["status"] = status;
+  loadTasks();
 }
 
 function highlight(id) {
-    document.getElementById(id).classList.add('drag-area-highlight');
+  document.getElementById(id).innerHTML = getDragTemplate();
 }
 
 function removeHighlight(id) {
-    document.getElementById(id).classList.remove('drag-area-highlight');
+  document.getElementById(id).innerHTML = "";
 }
 
-function getTaskTemplate(element){
-    return `<div class="filledContainer" draggable = "true" ondragstart="startDragging(${element['id']})">
+function getTaskTemplate(element) {
+  return `<div class="filledContainer" draggable = "true" ondragstart="startDragging(${element["id"]})">
               <div class="filledContainer__category">
                 <p>User Story</p>
               </div>
@@ -155,5 +176,25 @@ function getTaskTemplate(element){
                   <img src="../assets/icons/Prio media.png" alt="prio icon" />
                 </div>
               </div>
-            </div>`
+            </div>
+            
+            `;
+}
+
+function getDragTemplate() {
+  return `
+   <div class="emptyContainer">
+              <p class="emptyContainer__text">No Tasks To do</p>
+              <p></p>
+            </div>
+  `;
+}
+
+function getEmptyTemplate() {
+  return `
+   <div class="emptyContainer">
+              <p class="emptyContainer__text">No Tasks To do</p>
+              <p></p>
+            </div>
+  `;
 }
