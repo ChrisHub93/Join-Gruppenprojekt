e0f31acb779c54006036a30d9980981398c651c8
xCnKenan <cn.kenan@icloud.com>
2025-07-11 15:32:14 +0200

    user shown in board html

--- scripts/sign_up.js
+++ scripts/sign_up.js
@@ -22,6 +22,23 @@ async function getInpuValueAndPost() {
   let email = document.getElementById("signUpInputEmail").value;
   let password = document.getElementById("signUpInputPassword").value;
   await postData("/users/", { name: name, email: email, password: password, id:getId() });
+
+  // need to get logged in user to contacts json
+  let newUser = await fetchData("/users/");
+  let values = Object.values(newUser);
+  let findUser = values.find((currentUser)=> currentUser.name == name);
+  let fullName = name.split(" ");
+  let firstNameOfUser = fullName[0].charAt(0).toUpperCase(0) + fullName[0].slice(1);
+  let lastNameOfUser = fullName[1].charAt(0).toUpperCase(0) + fullName[1].slice(1);
+  if (findUser){
+    await postData("/contacts/", { firstname: firstNameOfUser, lastname: lastNameOfUser, email: email, password: password, id:findUser.id });
+  }
+}
+
+async function fetchData(path) {
+  let response = await fetch(BASE_URL + path + ".json");
+  let responseAsJson = await response.json();
+  return responseAsJson;
 }
 
 function getId() {
