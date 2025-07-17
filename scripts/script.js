d38e8e27d2a6468b1d42cd24607d38de9113007c
Dave Hannemann <david.dikschat@web.de>
2025-07-17 20:31:07 +0200

    sidebar behaviour fixed

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,7 @@ function logout() {
   sessionStorage.setItem("loginStatus", "none");
   sessionStorage.removeItem("loggedInUser");
   sessionStorage.removeItem("activeMenu");
+  sessionStorage.removeItem("activePolicy");
 }
 
 function sidebarVisibility() {
