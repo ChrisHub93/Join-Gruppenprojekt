dff4bceda2ab8c668d81856620c3d2a06793a3c8
xCnKenan <cn.kenan@icloud.com>
2025-07-26 14:29:16 +0200

    board.js. fixed problem with priority

--- scripts/board.js
+++ scripts/board.js
@@ -77,6 +77,18 @@ async function loadTasks() {
   }
 }
 
+function editOverlayTask(tasksRef) {  
+  taskOverlaySync();
+  let tasksEditRef = searchElement(tasksRef);
+  let addOverlayRef = document.getElementById("overlayTask");
+  let addOverlayEditRef = document.getElementById("overlayTaskEdit");
+  let dialogTaskEditRef = document.getElementById("dialogTaskEditContent");
+  addOverlayRef.classList.remove("active");
+  addOverlayEditRef.classList.add("active");
+  dialogTaskEditRef.innerHTML = renderOverlayTaskEdit(todos[tasksEditRef]);
+  upToDateEdit();  
+}
+
 function addEmptySubtasks() {
   todos = [];
 
