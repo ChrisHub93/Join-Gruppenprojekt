b06cb41b48df7cbea6798f82c66dd0b4b3a20f29
ChrisHub93 <chrisi.1993.mic@gmail.com>
2025-07-28 13:34:20 +0200

    contact_info.js: Added documentation

--- scripts/contacts_info.js
+++ scripts/contacts_info.js
@@ -1,3 +1,9 @@
+/**
+ * Removes detailed information about the currently active contact from the UI.
+ * Hides the contact info section, resets the active contact ID, and removes the contact's visual highlight.
+ *
+ * @param {Object} contact - The contact object whose information should be removed.
+ */
 function removeInfos(contact) {
   let allInfoAboutContactRef = document.getElementById("allInfoAboutContact");
   allInfoAboutContactRef.classList.remove("showAllInfoAboutContact");
@@ -6,10 +12,25 @@ function removeInfos(contact) {
   setNewBgForRef.remove();
 }
 
+/**
+ * Removes the main visual element representing the given contact from the DOM.
+ *
+ * @param {Object} contact - The contact object whose DOM element should be removed.
+ */
 function removeOldContactInfo(contact) {
   let targetSetNewBgForRef = document.getElementById("allMainInfoAbout" + contact.firstname + contact.lastname);
   targetSetNewBgForRef.remove();
 }
+
+/**
+ * Inserts the updated basic contact info into the appropriate alphabetical section in the DOM.
+ *
+ * @param {HTMLElement} divRef - The container element holding the contact's updated info.
+ * @param {string} firstName - The contact's first name.
+ * @param {string} lastName - The contact's last name.
+ * @param {HTMLElement} inputEmailRef - Reference to the email input field.
+ * @param {HTMLElement} inputPhoneRef - Reference to the phone input field.
+ */
 
 function getNewContactInfo(divRef,firstName,lastName,inputEmailRef,inputPhoneRef) {
   let alphabeticalOrderRef = document.getElementById("alphabeticalOrder" + firstName.charAt(0).toUpperCase());
